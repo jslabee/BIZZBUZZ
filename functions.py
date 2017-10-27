67c9a9295defd852b3aa501a2a55cfbe98725a78
jslabee <jslabe@gmail.com>
2017-10-27 13:16:37 +0200

    save

---
+++ functions.py
@@ -0,0 +1,15 @@
+def sum_num(a, b):
+    c = a + b
+
+    return c
+x = 3
+y = 2
+sum_ab = sum_num(x,y)
+print(sum_ab)
+
+def generate_list(a, b, c ,d):
+    newlist = []
+    newlist.append(a)
+    newlist.append(b)
+    newlist.append(c)
+    newlist.append(d)
---
+++ testg.py
@@ -0,0 +1,13 @@
+
+from math import sin
+
+class TestSumab(unittest.TestCase):
+    def testing_numbers_sum():
+        self.assertEqual(sum_sum(2, 3), 1)
+
+    def test_generate_list(self):
+        new_list = generate_list(1, 4, 6, 10)
+        self.assIsInstance(new_list, int)
+        self.assertIn(4, new_list)
+        self.assertisnot(newlist, none)
+        self.assertgreter(len(new_list), 0)
